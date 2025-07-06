5cbc6820239e6b119cec22feb4ed4b47d3487b82
Chenlinjia Yi <yichenlinjia@gmail.com>
2025-07-07 00:33:23 +0200

    Animacion idle

---
+++ Assets/Script/Animation/PetAnimationManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetAnimationManager : MonoBehaviour
+{
+    public static PetAnimationManager Instance { get; private set; }
+
+    private Animator animator;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            animator = GetComponent<Animator>();
+        }
+        else
+        {
+            Destroy(gameObject); 
+        }
+    }
+}
